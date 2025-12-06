61a12b7331beb6f9c35b9f2fc692865d0d1a2df4
gaboparra <geparra06@gmail.com>
2025-12-06 11:47:53 -0300

    prueba deploy

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  Swal.fire({
-    title: "¡Listo!",
-    text: "Mensaje enviado correctamente",
-    icon: "success",
-    confirmButtonText: "Aceptar",
-  });
-  this.reset();
+const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+  const html = document.documentElement;
+  const currentTheme = html.getAttribute("data-bs-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  html.setAttribute("data-bs-theme", newTheme);
 });
+
+const contactForm = document.getElementById("contactForm");
+if (contactForm) {
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    Swal.fire({
+      icon: "success",
+      title: "¡Mensaje Enviado!",
+      text: "Gracias por contactarme",
+    });
+    contactForm.reset();
+  });
+}
